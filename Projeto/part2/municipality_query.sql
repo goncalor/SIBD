4014171a9decf499d9203e4f802ea9ef8ca3af1d
rodrigomverissimo <rodrigomverissimo@gmail.com>
2015-11-08 19:11:15 +0000

    Updated 2nd query

--- Projeto/part2/municipality_query.sql
+++ Projeto/part2/municipality_query.sql
@@ -10,6 +10,9 @@ from Lives, Connects, Wears
 where Connects.manuf = 'Philips'
 and Connects.pan = Wears.pan
 and Wears.patient = Lives.patient
+and Connects.end >= NOW()
+and Wears.end >= NOW()
+and Lives.end >= NOW()
 group by muni
 having m_count >= all(select count(manuf)
 						from Lives, Connects, Wears
