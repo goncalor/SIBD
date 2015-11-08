ba7e4c9fb4d159fba97d58c3ee121390bc484844
rodrigomverissimo <rodrigomverissimo@gmail.com>
2015-11-08 18:41:29 +0000

    Added 2nd query

---
+++ Projeto/part2/municipality_query.sql
@@ -0,0 +1,22 @@
+drop procedure if exists municipality_query;
+
+delimiter $$
+
+create procedure municipality_query(out muni_out varchar(255))
+begin
+declare m_count integer;
+select muni, count(manuf) into muni_out, m_count
+from Lives, Connects, Wears
+where Connects.manuf = 'Philips'
+and Connects.pan = Wears.pan
+and Wears.patient = Lives.patient
+group by muni
+having m_count >= all(select count(manuf)
+						from Lives, Connects, Wears
+						where Connects.manuf = 'Philips'
+						and Connects.pan = Wears.pan
+						and Wears.patient = Lives.patient
+						group by muni);
+
+end$$
+delimiter ;
