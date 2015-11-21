68bbb1265d013aac4d646229523df776bbbac0df
rodrigomverissimo <rodrigomverissimo@gmail.com>
2015-11-21 17:09:51 +0000

    Fixed the 3rd query. Fully functional.

--- Projeto/part2/queryManufacturer.sql
+++ Projeto/part2/queryManufacturer.sql
@@ -1,24 +1,26 @@
--- Which manufacturers had devices described as "scale" being worn
--- last year in all municipalities covered by the medical centre?
-
-drop procedure if exists manufacturer;
+drop procedure if exists manufacturer_query;
 
 delimiter $$
 
-create procedure manufacturer()
+create procedure manufacturer_query()
 begin
-	select manufacturer as 'Manufacturer' -- the manufacturer was in all muni's, don't display muni?
-	from Device, Lives, Connects, Wears
-	where lower(description) like 'scale' 
-	and Device.serialnum = Connects.snum 
-	and Connects.pan = Wears.pan
-	and Wears.patient = Lives.patient
-	and Wears.end > DATE_SUB(NOW(), INTERVAL 1 YEAR) 
-	and Lives.end > DATE_SUB(NOW(), INTERVAL 1 YEAR)
-	and Wears.start <= Lives.end -- we also have to guarantee that the wear and live dates overlap
-	and Wears.end >= Lives.end
-	group by manufacturer -- check if each manufacturer was present in all muni's
-	having count(distinct Lives.muni) = (select count(nut4code) from Municipality);
+select distinct manufacturer
+from Device as d
+where not exists(
+	select nut4code
+	from Municipality
+	where nut4code not in(
+		select muni
+		from Connects, Device as d2, Wears, Lives
+		where description like 'scale'
+		and d2.serialnum = Connects.snum
+		and Connects.pan = Wears.pan
+		and Wears.patient = Lives.patient
+		and d2.manufacturer = d.manufacturer
+		and Wears.end > DATE_SUB(NOW(), INTERVAL 1 YEAR) 
+and Lives.end > DATE_SUB(NOW(), INTERVAL 1 YEAR)
+and Connects.end > DATE_SUB(NOW(), INTERVAL 1 YEAR)));
+
 end$$
 
 delimiter ;
