1d73042df6a320cde9557be04b094981204dae8f
rodrigomverissimo <rodrigomverissimo@gmail.com>
2015-11-22 21:59:18 +0000

    Removed the possibility of searching incomplete inputs.

--- Projeto/part2/display_all_readings.sql
+++ Projeto/part2/display_all_readings.sql
@@ -6,7 +6,7 @@ create procedure display_all_readings(in p_name varchar(255))
 begin
 	select Patient.number, Reading.datetime, Reading.snum, Reading.manuf, Sensor.units, Reading.value
 	from Patient, Wears, Connects, Reading, Sensor
-	where Patient.name like p_name
+	where Patient.name = p_name
 	and Patient.number = Wears.patient
 	and Wears.pan = Connects.pan
 	and Connects.snum = Reading.snum
--- Projeto/part2/display_all_settings.sql
+++ Projeto/part2/display_all_settings.sql
@@ -6,7 +6,7 @@ create procedure display_all_settings(in p_name varchar(255))
 begin
 select Patient.number, Setting.datetime, Setting.snum, Setting.manuf, Actuator.units, Setting.value
 from Patient, Wears, Connects, Setting, Actuator
-where Patient.name like p_name
+where Patient.name = p_name
 and Patient.number = Wears.patient
 and Wears.pan = Connects.pan
 and Connects.snum = Setting.snum
