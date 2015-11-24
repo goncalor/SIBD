2221aa872ff5664646530b1cb92b44e22d5e6551
rodrigomverissimo <rodrigomverissimo@gmail.com>
2015-11-24 17:21:45 +0000

    Perfected the first query

--- Projeto/part2/queryReadings.sql
+++ Projeto/part2/queryReadings.sql
@@ -8,14 +8,17 @@ delimiter $$
 
 create procedure queryReadings()
 begin
-	select distinct Wears.patient as 'Patient ID', Reading.snum, Reading.manuf, datetime, Reading.value, Device.description
-	from Reading, Wears, Connects, Device
+	select distinct Wears.patient as 'Patient ID', Reading.snum, Reading.manuf, datetime, Sensor.units,Reading.value, Device.description
+	from Reading, Wears, Connects, Device, Sensor
 	where Wears.pan = Connects.pan 
 	and Connects.snum = Reading.snum 
 	and Connects.manuf = Reading.manuf 
 	and Connects.snum = Device.serialnum 
 	and Connects.manuf = Device.manufacturer 
+	and Connects.snum = Sensor.snum
 	and Reading.datetime BETWEEN DATE_SUB(NOW(), INTERVAL 6 MONTH) AND NOW()
+	and Reading.datetime BETWEEN Connects.start and Connects.end
+	and Reading.datetime BETWEEN Wears.start and Wears.end
 	and lower(Device.description) like 'blood pressure'
 	order by Wears.patient;
 end$$
