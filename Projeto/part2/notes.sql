14d6f46b5413375689ec56b8ed6f37d2454ede02
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-11-25 20:11:20 +0000

    Delete notes.sql

--- Projeto/part2/notes.sql
+++
@@ -1,3 +0,0 @@
-select start, end, snum, manuf, pan from Connects where pan = 'www.ist1111.com';
-update Connects set start='2015-11-23 16:36:45', end='2099-01-01 00:00:00', pan='www.ist4444.com' where snum='B3333' and manuf='Aki';
-select * from Period orderby start desc;
